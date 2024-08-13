d0ece676d703fb567f3d6d02c3f3f828b473aa4e
MchKosticyn <mishakosticyn@yandex.ru>
2024-08-13 16:49:02 +0300

    updated approximations version

--- usvm-jvm/build.gradle.kts
+++ usvm-jvm/build.gradle.kts
@@ -1,24 +1,12 @@
 @file:Suppress("PropertyName", "HasPlatformType")
 
-import kotlin.io.path.Path
-import kotlin.io.path.absolutePathString
-import kotlin.io.path.createDirectories
-
-
 plugins {
     id("usvm.kotlin-conventions")
-    id("org.springframework.boot") version "3.2.0"
 }
 
 val samples by sourceSets.creating {
     java {
         srcDir("src/samples/java")
-    }
-}
-
-val `samples-jdk11` by sourceSets.creating {
-    java {
-        srcDir("src/samples-jdk11/java")
     }
 }
 
@@ -35,25 +23,18 @@ val `usvm-api` by sourceSets.creating {
 }
 
 val approximations by configurations.creating
-val approximationsRepo = "org.usvm.approximations.java.stdlib"
-val approximationsVersion = "0.0.0"
-
-repositories {
-    mavenLocal()
-    mavenCentral()
-}
+val approximationsRepo = "com.github.UnitTestBot.java-stdlib-approximations"
+val approximationsVersion = "5f137507d6"
 
 dependencies {
     implementation(project(":usvm-core"))
     implementation(project(":usvm-jvm-dataflow"))
-    implementation(project(":usvm-jvm:usvm-jvm-util"))
 
     implementation(Libs.jacodb_api_jvm)
     implementation(Libs.jacodb_core)
     implementation(Libs.jacodb_approximations)
 
     implementation(`usvm-api`.output)
-    testImplementation(project("usvm-jvm-test-api"))
 
     implementation(Libs.ksmt_runner)
     implementation(Libs.ksmt_yices)
@@ -74,19 +55,9 @@ dependencies {
     testImplementation(approximationsRepo, "tests", approximationsVersion)
 }
 
-val springApproximationsDeps by configurations.creating
-
-dependencies {
-    springApproximationsDeps("org.springframework.boot:spring-boot-starter-test:3.2.0")
-    springApproximationsDeps("org.springframework.boot:spring-boot-starter-web:3.2.0")
-    springApproximationsDeps("org.springframework:spring-jcl:6.1.1")
-    springApproximationsDeps("org.springframework.boot:spring-boot-starter-data-jpa:3.2.0")
-}
-
 val `usvm-apiCompileOnly`: Configuration by configurations.getting
 dependencies {
     `usvm-apiCompileOnly`(Libs.jacodb_api_jvm)
-    implementation(project(":usvm-jvm:usvm-jvm-util"))
 }
 
 val samplesImplementation: Configuration by configurations.getting
@@ -121,20 +92,9 @@ val `usvm-api-jar` = tasks.register<Jar>("usvm-api-jar") {
 val testSamples by configurations.creating
 val testSamplesWithApproximations by configurations.creating
 
-val compileSamplesJdk11 = tasks.register<JavaCompile>("compileSamplesJdk11") {
-    sourceCompatibility = JavaVersion.VERSION_11.toString()
-    targetCompatibility = JavaVersion.VERSION_11.toString()
-
-    source = `samples-jdk11`.java
-    classpath = `samples-jdk11`.compileClasspath
-    options.sourcepath = `samples-jdk11`.java
-    destinationDirectory = `samples-jdk11`.java.destinationDirectory
-}
-
 dependencies {
     testSamples(samples.output)
     testSamples(`usvm-api`.output)
-    testSamples(files(`samples-jdk11`.java.destinationDirectory))
 
     testSamplesWithApproximations(samples.output)
     testSamplesWithApproximations(`usvm-api`.output)
@@ -144,7 +104,7 @@ dependencies {
 
 tasks.withType<Test> {
     dependsOn(`usvm-api-jar`)
-    dependsOn(compileSamplesJdk11, testSamples, testSamplesWithApproximations)
+    dependsOn(testSamples, testSamplesWithApproximations)
 
     val usvmApiJarPath = `usvm-api-jar`.get().outputs.files.singleFile
     val usvmApproximationJarPath = approximations.resolvedConfiguration.files.single()
@@ -210,146 +170,4 @@ publishing {
             artifact(`usvm-api-jar`)
         }
     }
-}
-
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web:3.3.4")
-    implementation("org.springframework.boot:spring-boot-starter-test:3.3.4")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.4")
-    implementation("org.apache.xmlbeans:xmlbeans:5.2.1")
-}
-
-tasks.register<JavaExec>("runWebBench") {
-    mainClass.set("bench.WebBenchKt")
-    classpath = sourceSets.test.get().runtimeClasspath
-
-    dependsOn(`usvm-api-jar`)
-
-    val usvmApiJarPath = `usvm-api-jar`.get().outputs.files.singleFile
-    val usvmApproximationJarPath = approximations.resolvedConfiguration.files.single()
-    val springApproximationDepsJarPath = springApproximationsDeps.resolvedConfiguration.files
-    val absolutePaths = springApproximationDepsJarPath.joinToString(";") { it.absolutePath }
-
-    // TODO: norm? #CM #Valya
-    systemProperty("usvm.jvm.springApproximationsDeps.paths", absolutePaths)
-    val currentDir = Path(System.getProperty("user.dir"))
-    val generatedDir = currentDir.resolve("generated")
-    generatedDir.createDirectories()
-    systemProperty("generatedDir", generatedDir.absolutePathString())
-    val lambdaDir = generatedDir.resolve("lambdas")
-    lambdaDir.createDirectories()
-    systemProperty("lambdasDir", lambdaDir.absolutePathString())
-
-    environment("usvm.jvm.api.jar.path", usvmApiJarPath.absolutePath)
-    environment("usvm.jvm.approximations.jar.path", usvmApproximationJarPath.absolutePath)
-
-    jvmArgs = listOf("-Xmx10g") + mutableListOf<String>().apply {
-        add("-Djava.security.manager -Djava.security.policy=webExplorationPolicy.policy")
-        add("-Djdk.internal.lambda.dumpProxyClasses=${lambdaDir.absolutePathString()}")
-        openPackage("java.base", "jdk.internal.misc")
-        openPackage("java.base", "java.lang")
-        openPackage("java.base", "java.lang.reflect")
-        openPackage("java.base", "sun.security.provider")
-        openPackage("java.base", "jdk.internal.event")
-        openPackage("java.base", "jdk.internal.jimage")
-        openPackage("java.base", "jdk.internal.jimage.decompressor")
-        openPackage("java.base", "jdk.internal.jmod")
-        openPackage("java.base", "jdk.internal.jtrfs")
-        openPackage("java.base", "jdk.internal.loader")
-        openPackage("java.base", "jdk.internal.logger")
-        openPackage("java.base", "jdk.internal.math")
-        openPackage("java.base", "jdk.internal.misc")
-        openPackage("java.base", "jdk.internal.module")
-        openPackage("java.base", "jdk.internal.org.objectweb.asm.commons")
-        openPackage("java.base", "jdk.internal.org.objectweb.asm.signature")
-        openPackage("java.base", "jdk.internal.org.objectweb.asm.tree")
-        openPackage("java.base", "jdk.internal.org.objectweb.asm.tree.analysis")
-        openPackage("java.base", "jdk.internal.org.objectweb.asm.util")
-        openPackage("java.base", "jdk.internal.org.xml.sax")
-        openPackage("java.base", "jdk.internal.org.xml.sax.helpers")
-        openPackage("java.base", "jdk.internal.perf")
-        openPackage("java.base", "jdk.internal.platform")
-        openPackage("java.base", "jdk.internal.ref")
-        openPackage("java.base", "jdk.internal.reflect")
-        openPackage("java.base", "jdk.internal.util")
-        openPackage("java.base", "jdk.internal.util.jar")
-        openPackage("java.base", "jdk.internal.util.xml")
-        openPackage("java.base", "jdk.internal.util.xml.impl")
-        openPackage("java.base", "jdk.internal.vm")
-        openPackage("java.base", "jdk.internal.vm.annotation")
-        openPackage("java.base", "java.util.concurrent.atomic")
-        openPackage("java.base", "java.io")
-        openPackage("java.base", "java.util.zip")
-        openPackage("java.base", "java.util.concurrent")
-        openPackage("java.base", "sun.security.util")
-        openPackage("java.base", "java.lang.invoke")
-        openPackage("java.base", "java.lang.ref")
-        openPackage("java.base", "java.lang.constant")
-        openPackage("java.base", "java.util")
-        openPackage("java.base", "java.util.concurrent.locks")
-        openPackage("java.management", "javax.management")
-        openPackage("java.base", "java.nio.charset")
-        openPackage("java.base", "java.util.regex")
-        openPackage("java.base", "java.net")
-        openPackage("java.base", "sun.util.locale")
-        openPackage("java.base", "java.util.stream")
-        openPackage("java.base", "java.security")
-        openPackage("java.base", "java.time")
-        openPackage("java.base", "jdk.internal.access")
-        openPackage("java.base", "sun.reflect.annotation")
-        openPackage("java.base", "sun.reflect.generics.reflectiveObjects")
-        openPackage("java.base", "sun.reflect.generics.factory")
-        openPackage("java.base", "sun.reflect.generics.tree")
-        openPackage("java.base", "sun.reflect.generics.scope")
-        openPackage("java.base", "sun.invoke.util")
-        openPackage("java.base", "sun.nio.cs")
-        openPackage("java.base", "sun.nio.fs")
-        openPackage("java.base", "java.nio")
-        openPackage("java.logging", "java.util.logging")
-        openPackage("java.base", "java.time.format")
-        openPackage("java.base", "java.time.zone")
-        openPackage("java.base", "java.time.temporal")
-        openPackage("java.base", "java.text")
-        openPackage("java.base", "sun.util.calendar")
-        openPackage("java.base", "sun.net.www.protocol.jar")
-        openPackage("java.base", "java.util.jar")
-        openPackage("java.base", "java.nio.file.attribute")
-        openPackage("java.base", "java.util.function")
-        openPackage("java.desktop", "java.beans")
-        openPackage("java.xml", "com.sun.org.apache.xerces.internal.impl.xs")
-        openPackage("java.base", "java.math")
-        openPackage("java.base", "java.nio.file")
-        openPackage("java.base", "java.nio.channels")
-        openPackage("java.base", "javax.net.ssl")
-        openPackage("java.base", "java.lang.annotation")
-        openPackage("java.base", "java.lang.runtime")
-        openPackage("java.base", "javax.crypto")
-        exportPackage("java.base", "sun.util.locale")
-        exportPackage("java.base", "jdk.internal.misc")
-        exportPackage("java.base", "jdk.internal.reflect")
-        exportPackage("java.base", "sun.nio.cs")
-        exportPackage("java.xml", "com.sun.org.apache.xerces.internal.impl.xs.util")
-        add("--illegal-access=warn")
-        add("-XX:+UseParallelGC")
-    }
-}
-
-fun MutableList<String>.openPackage(module: String, pakage: String) {
-    add("--add-opens")
-    add("$module/$pakage=ALL-UNNAMED")
-}
-
-fun MutableList<String>.exportPackage(module: String, pakage: String) {
-    add("--add-exports")
-    add("$module/$pakage=ALL-UNNAMED")
-}
-
-fun JavaExec.addEnvIfExists(envName: String, path: String) {
-    val file = File(path)
-    if (!file.exists()) {
-        println("Not found $envName at $path")
-        return
-    }
-
-    environment(envName, file.absolutePath)
 }
